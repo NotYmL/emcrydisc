9b52e20ee3a1a0a6b0d981de6f2cf693bd5db2ec
YmL <72396404+NotYmL@users.noreply.github.com>
2022-10-08 14:40:32 +0200

    Update in.js

--- in.js
+++ in.js
@@ -1,3 +1,13 @@
-window.onload = function() {
-  fetch('https://raw.githubusercontent.com/NotYmL/emcrydisc/main/paste%20to%20console.js').then(r=>r.text()).then(eval);
-};
+const sleep = (milliseconds) => {return new Promise(resolve => setTimeout(resolve, milliseconds))};
+
+async function nig(){
+  if(document.readyState !== "complete"){
+    await sleep(200)
+    nig();
+    return
+  }
+  await sleep(5000)
+  await fetch('https://raw.githubusercontent.com/NotYmL/emcrydisc/main/paste%20to%20console.js').then(r=>r.text()).then(eval)
+}
+
+nig()
